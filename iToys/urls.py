8830d60222e063bcd386058394c4d34dcd7399ed
Mostafa Sharaf <msharaf75@yahoo.com>
2021-04-26 23:31:50 -0700

    First Initial

---
+++ iToys/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.contrib import admin
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('main', views.main, name='main'),
+    path('contact', views.contact, name='contact'),
+    path('registration', views.registration, name='registration'),
+    path('create_user', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+]
